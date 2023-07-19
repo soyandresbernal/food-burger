2b115d1d3323f8fdb4b74a21d182a8247af95594
Andrés <soyandresbernal@gmail.com>
2023-07-19 18:01:56 -0500

    feat: add images

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,59 +1,63 @@
-import { NextResponse } from 'next/server'
-import { NextRequest } from 'next/server'
-
-const jwt = require ('jwt-simple');
+import { NextResponse } from 'next/server';
+const jwt = require('jwt-simple');
 const moment = require('moment');
 const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcryptjs');
-const database = new sqlite3.Database("db/my.db");
+const database = new sqlite3.Database('db/my.db');
 
 const createUserTable = () => {
-  const query = `
+	const query = `
       CREATE TABLE IF NOT EXISTS users (
       id integer PRIMARY KEY,
       fullname text,
       password text,
       cellphone text,
       email text UNIQUE)`;
-  return database.run(query);
-}
+	return database.run(query);
+};
 createUserTable();
 
 export async function POST(req, res) {
-  const body = await req.json(); 
-  const {email, password} = body;
-  const user = await getUserEmail(email)
-  if (user){
-    const validatePassword = bcrypt.compareSync(password, user.password);
-    if (validatePassword){
-      user.token = createToken(user);
-      return NextResponse.json(user)
-    } else {
-      return NextResponse.json({"error": "Wrong password"}, { status: 401 });
-    }
-
-  } else {
-    return NextResponse.json({"error": "The email does not exists"}, { status: 404 });
-  }
+	const body = await req.json();
+	const { email, password } = body;
+	const user = await getUserEmail(email);
+	if (user) {
+		const validatePassword = bcrypt.compareSync(password, user.password);
+		if (validatePassword) {
+			user.token = createToken(user);
+			return NextResponse.json(user);
+		} else {
+			return NextResponse.json({ error: 'Wrong password' }, { status: 401 });
+		}
+	} else {
+		return NextResponse.json(
+			{ error: 'The email does not exists' },
+			{ status: 404 }
+		);
+	}
 }
 
 const createToken = (user) => {
-  const payload = {
-      userId: user.id,
-      createdAt: moment().unix(),
-      expiredAt: moment().add(5, 'minutes').unix()
-  }
+	const payload = {
+		userId: user.id,
+		createdAt: moment().unix(),
+		expiredAt: moment().add(5, 'minutes').unix()
+	};
 
-  return jwt.encode(payload, 'secret string')
-}
+	return jwt.encode(payload, 'secret string');
+};
 
-function getUserEmail(email){
-  return new Promise((resolve, reject) => {
-    database.all("SELECT * FROM users WHERE email = (?);", [email], function (err, rows) {
-      if (err) {
-        reject({});
-      }
-      resolve(rows[0]);
-    });
-  });
+function getUserEmail(email) {
+	return new Promise((resolve, reject) => {
+		database.all(
+			'SELECT * FROM users WHERE email = (?);',
+			[email],
+			function (err, rows) {
+				if (err) {
+					reject({});
+				}
+				resolve(rows[0]);
+			}
+		);
+	});
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 export default function Home() {
-  return (
-    <><h1>Welcome to Food Burger</h1>
-    <p><a href='login'>Login</a> to continue</p></>
-  )
+	return (
+		<>
+			<h1>Welcome to Food Burger</h1>
+			<p>
+				<a href='login'>Login</a> to continue
+			</p>
+			<h1 className='text-3xl font-bold underline'>Hello, test front!</h1>
+		</>
+	);
 }
